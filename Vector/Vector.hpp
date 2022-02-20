08f0b4bfa8f2f9e43c2890f2dce923d1c85dacb8
Reaper Lando <rlando@mi-e3.msk.21-school.ru>
2022-02-20 20:40:36 +0300

    start of vector implementation

--- Vector/Vector.hpp
+++ Vector/Vector.hpp
@@ -5,4 +5,18 @@
 #ifndef CONTAINERS_VECTOR_HPP
 #define CONTAINERS_VECTOR_HPP
 
+namespace ft {
+    template<class T, class Alloc = std::allocator<T> >
+    class vector {
+    private:
+        typedef T value_type;
+        typedef Alloc allocator_type;
+        typedef size_t size_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+    };
+}
+
 #endif //CONTAINERS_VECTOR_HPP
