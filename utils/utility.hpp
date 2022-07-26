0eddf2e37e150471cf058d33fbd4741b13cdd192
Said Islamov <urumqinsky@gmail.com>
2022-07-26 22:09:07 +0300

    Create pair & make_pair

---
+++ utils/utility.hpp
@@ -0,0 +1,68 @@
+//
+// Created by Said Islamov on 15.07.2022.
+//
+
+#ifndef CONTAINERS_UTILITY_HPP
+#define CONTAINERS_UTILITY_HPP
+
+namespace ft {
+	template<class T1, class T2>
+	struct pair {
+		typedef T1 first_type;
+		typedef T2 second_type;
+
+		T1 first;
+		T2 second;
+
+		pair() : first(T1()), second(T2()) {}
+
+		pair(const T1& x, const T2& y) : first(x), second(y) {}
+
+		template<class U1, class U2>
+		pair(const pair<U1, U2>& x) : first(x.first), second(x.second) {}
+
+		pair& operator=(const pair& other) {
+			if (this != &other) {
+				first = other.first;
+			}
+			return *this;
+		}
+	};
+
+	template<class T1, class T2>
+	pair<T1, T2> make_pair(const T1& x, const T2& y) {
+		return pair<T1, T2>(x, y);
+	}
+
+	template<class T1, class T2>
+	bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return lhs.first == rhs.first && lhs.second == rhs.second;
+	}
+
+	template<class T1, class T2>
+	bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return !(lhs == rhs);
+	}
+
+	template<class T1, class T2>
+	bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return lhs.first < rhs.first || !(rhs.first < lhs.first && lhs.second < rhs.second);
+	}
+
+	template<class T1, class T2>
+	bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return rhs < lhs;
+	}
+
+	template<class T1, class T2>
+	bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return !(rhs < lhs);
+	}
+
+	template<class T1, class T2>
+	bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return !(lhs < rhs);
+	}
+}
+
+#endif //CONTAINERS_UTILITY_HPP
