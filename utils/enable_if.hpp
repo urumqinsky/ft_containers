1bdd75c07c2258a936c3c50c41a18931a4497043
Said Islamov <urumqinsky@gmail.com>
2022-07-26 22:05:32 +0300

    Update enable_if

--- utils/enable_if.hpp
+++ utils/enable_if.hpp
@@ -16,7 +16,7 @@ namespace ft {
 		typedef T type;
 	};
 
-	template <class T, T v>
+	template<class T, T v>
 	struct integral_constant {
 		static const T value = v;
 		typedef T value_type;
@@ -26,49 +26,49 @@ namespace ft {
 		}
 	};
 
-	template <typename T>
+	template<typename T>
 	struct is_integral : public integral_constant<bool, false> {};
 
-	template <>
+	template<>
 	struct is_integral<char> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<char16_t> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<char32_t> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<wchar_t> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<signed char> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<short int> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<int> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<long int> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<long long int> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<unsigned char> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<unsigned short int> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<unsigned int> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<unsigned long int> : public integral_constant<bool, true> {};
 
-	template <>
+	template<>
 	struct is_integral<unsigned long long int> : public integral_constant<bool, true> {};
 }
 
